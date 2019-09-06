1ba87c92ecaca287e1dcd67280712c90eee25970
Alexander Panteleev <746590@mail.ru>
2019-09-06 16:08:30 +0300

    add header, tiny style updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import AddTaskBar from './components/AddTaskBar'
+import AddTaskBar from './components/AddTaskBar';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AddTaskBar />
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="main-header">
+          <h1 className="main-header__title">
+            <i className="list alternate outline icon"></i>
+            Todo App
+          </h1>
+          <a href="https://github.com/flymans"><i class="huge github icon"></i></a>
+      </header>
+    )
+  }
+}
+
+export default Header;
