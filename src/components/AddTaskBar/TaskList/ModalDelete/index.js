9388ac1ac0a209abe4b4f01f845d5469a9c6bda9
Alexander Panteleev <746590@mail.ru>
2019-09-24 01:43:24 +0300

    minor modal improvement using state hook

--- src/components/AddTaskBar/TaskList/ModalDelete/index.js
+++ src/components/AddTaskBar/TaskList/ModalDelete/index.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {Button, Header, Icon, Modal} from 'semantic-ui-react';
 
-const ModalDelete = ({deleteTask, itemId}) => (
-    <Modal
-        trigger={<Button type="button" circular floated="right" icon="trash" />}
-        closeIcon
-    >
-        <Header icon="trash" content="Delete task" />
-        <Modal.Content>
-            <p>Are you sure you want to delete this item?</p>
-        </Modal.Content>
-        <Modal.Actions>
-            <Button color="green" inverted onClick={deleteTask(itemId)}>
-                <Icon name="checkmark" /> Yes
-            </Button>
-        </Modal.Actions>
-    </Modal>
-);
+const ModalDelete = ({deleteTask, itemId}) => {
+    const [modal, handleModal] = useState({modalOpen: false});
+
+    const handleOpen = () => handleModal({...modal, modalOpen: true});
+    const handleClose = () => handleModal({...modal, modalOpen: false});
+
+    return (
+        <Modal
+            trigger={
+                <Button
+                    onClick={handleOpen}
+                    type="button"
+                    circular
+                    floated="right"
+                    icon="trash"
+                />
+            }
+            closeIcon
+            open={modal.modalOpen}
+            onClose={handleClose}
+        >
+            <Header icon="trash" content="Delete task" />
+            <Modal.Content>
+                <p>Are you sure you want to delete this item?</p>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button onClick={handleClose} color="red" inverted>
+                    <Icon name="remove" />
+                    No
+                </Button>
+                <Button color="green" inverted onClick={deleteTask(itemId)}>
+                    <Icon name="checkmark" /> Yes
+                </Button>
+            </Modal.Actions>
+        </Modal>
+    );
+};
 
 ModalDelete.propTypes = {
     deleteTask: PropTypes.func.isRequired,
