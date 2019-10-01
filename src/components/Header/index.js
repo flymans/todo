3962bbd102ffe94c7c317cda23113fd33cdc8e08
Alexander Panteleev <746590@mail.ru>
2019-10-01 23:57:43 +0300

    use css grid

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-  <header className="main-header">
-    <h1 className="main-header__title">
-      <i className="list alternate outline icon" />
-          Todo App
-    </h1>
-    <a href="https://github.com/flymans">
-      <i className="huge github icon" />
-    </a>
-  </header>
+    <header className="main-header">
+        <h1 className="main-header__title">
+            <i className="list alternate outline icon" />
+            Todo App
+        </h1>
+        <div className="main-header__logo">
+            <a href="https://github.com/flymans">
+                <i className="huge github icon" />
+            </a>
+        </div>
+    </header>
 );
 export default Header;
